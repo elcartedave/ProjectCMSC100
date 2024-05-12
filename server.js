6d36eb193fbf74a42111168f133ea3843870a0f5
sadiares1 <159208661+sadiares1@users.noreply.github.com>
2024-05-12 16:44:50 +0000

    feat(/signup): implement signup

---
+++ server.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import mongoose from "mongoose";
+import validator from 'validator';
+
+const app = express();
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json()); //this use as converting format info in json data
+await mongoose.connect('mongodb+srv://sadiares1:hKgPfm6gaefBBSm1@cluster0.namen2s.mongodb.net/ICS');
+
+//create model
+const userDataSchema = {
+    firstName: String,
+    lastName: String,
+    userType: String,
+    eMail: String,
+    passWord: String
+}
+
+const signUp = mongoose.model("userData", userDataSchema);
+
+app.get("/", function(req, res){
+    res.sendFile( "/workspaces/100p/index.html");
+})
+
+//app post
+app.post("/", async function(req, res){
+    var empty = "";
+    var merchant = "merchant";
+    var customer = "customer";
+    if(req.body.fname != empty && req.body.lname != empty && req.body.userType != empty && req.body.password != empty && req.body.email != empty){
+        let ut = req.body.userType.toLowerCase();
+        if((ut == merchant || ut == customer) && validator.isEmail(req.body.email)){
+            let newsignUP = new signUp({
+                firstName: req.body.fname,
+                lastName: req.body.lname,
+                userType: req.body.userType,
+                eMail: req.body.email,
+                passWord: req.body.password
+            });
+            await newsignUP.save()
+                .then(()=>{
+                    res.send("Account created successfully");
+                })
+                .catch((err)=>{
+                    res.status(500).send("Error creating account: " + err.message);
+                });
+        }else{
+        res.status(400).send("Invalid data provided");
+        }
+    }
+    else{
+        res.status(400).send("Incomplete data provided");
+    }
+});
+
+
+
+app.listen(3000, function(){
+    console.log("server is running");
+})
