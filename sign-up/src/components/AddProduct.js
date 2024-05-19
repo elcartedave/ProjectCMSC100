d7cbb81803a2b9ed72b31c8b8755199bf15e31f8
denmt <rgcomia@up.edu.ph>
2024-05-19 17:16:44 +0800

    feat: Add AddProduct component for admin to add new products

--- sign-up/src/components/AddProduct.js
+++ sign-up/src/components/AddProduct.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import "./CSS/AddProduct.css";
+import { useNavigate } from "react-router-dom";
 const AddProduct = () => {
+  const navigate = useNavigate();
   const [image, setImage] = useState(false);
   const [productDetails, setProductDetails] = useState({
     name: "",
@@ -61,7 +64,7 @@ const AddProduct = () => {
               quantity: "",
             });
             setImage(false);
-            window.scrollTo(0, 0);
+            navigate("/listproduct");
           } else {
             alert("Failed!");
           }
@@ -70,94 +73,92 @@ const AddProduct = () => {
   };
 
   return (
-    <div className="addproduct">
+    <div className="add-product">
+      <h1 className="admin-header">ADD A PRODUCT</h1>
       <div className="addproduct-itemfield">
-        <p>Product Name</p>
+        <p className="input-label">Product Name</p>
         <input
           value={productDetails.name}
           onChange={changeHandler}
           type="text"
           name="name"
+          className="product-input"
           placeholder="Type Here"
           autocomplete="off"
         />
-      </div>
-      <div className="addproduct-price">
-        <div className="addproduct-itemfield">
-          <p>Price</p>
+          <p className="input-label">Price</p>
           <input
             value={productDetails.price}
             onChange={changeHandler}
             type="text"
             name="price"
+            className="product-input"
             placeholder="Type Here"
             autocomplete="off"
           />
-        </div>
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Product Type</p>
+        <p className="input-label">Product Type</p>
         <select
           value={productDetails.type}
           onChange={changeHandler}
           name="type"
-          className="add-product-selector"
+          className="select-type"
         >
           <option value="crop">Crop</option>
           <option value="poultry">Poultry</option>
-        </select>
-      </div>
-      <div className="addproduct-itemfield">
-        <label htmlFor="file-input">
-          <img
+        </select>       
+        <p className="image-label">Product Image</p>
+        <img
             src={
               image
                 ? URL.createObjectURL(image)
-                : "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/upload-512.png"
+                : "./images/add-image.png"
             }
             alt=""
-            className="addproduct-thumnail-img"
+            className="product-thumbnail"
           />
-        </label>
+        <br />
+        <label htmlFor="file-input" className="product-image"> <i class='bx bx-image-add' ></i> ADD IMAGE</label>
         <input
           onChange={imageHandler}
+          className="image-input"
           type="file"
           name="image"
           id="file-input"
           hidden
         />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Description</p>
+        <p className="input-label">Description</p>
         <input
           value={productDetails.description}
           onChange={changeHandler}
           name="description"
+          className="product-input"
           type="text"
           placeholder="Type Here"
           autocomplete="off"
         />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Quantity</p>
+        <p className="input-label">Quantity</p>
         <input
           value={productDetails.quantity}
           onChange={changeHandler}
           type="number"
           placeholder="Type Here"
           name="quantity"
+          className="product-input"
           autocomplete="off"
         />
-      </div>
+      <div>
       <button
         onClick={() => {
           Add_Product();
           console.log("Image" + image);
         }}
-        className="addproduct-btn"
+        className="add-button"
       >
-        ADD
+        <i class='bx bx-check'></i>
+          ADD PRODUCT
       </button>
+      </div>
+      </div>
     </div>
   );
 };
