67e31a456be6a71298ff9ee1512c2e5e6bf00b89
denmt <rgcomia@up.edu.ph>
2024-05-30 01:49:31 +0800

    chore: Update npm dependencies and add charting libraries for sales report

--- sign-up/src/components/SalesReport.js
+++ sign-up/src/components/SalesReport.js
@@ -1,62 +1,60 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import chroma from 'chroma-js';
+import 'chartjs-plugin-datalabels';
+import "./CSS/SalesReport.css";
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const SalesReport = () => {
   const [salesReport, setSalesReport] = useState([]);
   const [totalSalesAmount, setTotalSalesAmount] = useState(0);
   const [totalSalesQuantity, setTotalSalesQuantity] = useState(0);
-  const [timePeriod, setTimePeriod] = useState("allTime"); //show the instance as all time as it is initialize
+  const [timePeriod, setTimePeriod] = useState("allTime");
 
   useEffect(() => {
     fetchSalesReport();
-  }, [timePeriod]); //render the data on which time period the admin wants
+  }, [timePeriod]);
 
   const fetchSalesReport = async () => {
     try {
       const { startDate, endDate } = getDateRange(timePeriod);
       const response = await axios.get("http://localhost:3001/salesreport", {
         params: { startDate, endDate },
-      }); //sends the starting date and end date
+      });
       setSalesReport(response.data);
-      calculateTotals(response.data); //calc total sales of a certain product then total it
+      calculateTotals(response.data);
     } catch (error) {
       console.error("Failed to fetch sales report:", error);
     }
   };
 
   const getDateRange = (period) => {
-    // Create a new Date object representing the current date and time
     const now = new Date();
     let startDate, endDate;
 
-    // Determine the start and end date based on the specified period
     switch (period) {
       case "weekly":
-        // Set startDate to the first day of the current week (Sunday)
         startDate = new Date(now.setDate(now.getDate() - now.getDay()));
-        // Set endDate to the last day of the current week (Saturday)
         endDate = new Date(now.setDate(now.getDate() + 6 - now.getDay()));
         break;
       case "monthly":
-        // Set startDate to the first day of the current month
         startDate = new Date(now.getFullYear(), now.getMonth(), 1);
-        // Set endDate to the last day of the current month
         endDate = new Date(now.getFullYear(), now.getMonth() + 1, 0);
         break;
       case "yearly":
-        // Set startDate to the first day of the current year
         startDate = new Date(now.getFullYear(), 0, 1);
-        // Set endDate to the last day of the current year
         endDate = new Date(now.getFullYear(), 11, 31);
         break;
       default:
-        // If no valid period is provided, set startDate and endDate to null
         startDate = null;
         endDate = null;
         break;
     }
 
-    // Return the start and end dates in ISO string format
     return {
       startDate: startDate ? startDate.toISOString() : null,
       endDate: endDate ? endDate.toISOString() : null,
@@ -64,7 +62,6 @@ const SalesReport = () => {
   };
 
   const calculateTotals = (report) => {
-    //calculate total by gettung total amount of each item and total quantity sales of each item
     let totalAmount = 0;
     let totalQuantity = 0;
     report.forEach((item) => {
@@ -73,6 +70,32 @@ const SalesReport = () => {
     });
     setTotalSalesAmount(totalAmount);
     setTotalSalesQuantity(totalQuantity);
+  };
+
+  const chartProductQuantity = {
+    labels: salesReport.map(item => item.productName),
+    datasets: [
+      {
+        label: 'Total Sales Quantity',
+        data: salesReport.map(item => item.totalSalesQuantity),
+        labels: salesReport.map(item => item.productName),
+        backgroundColor: chroma.scale(['#1e4f2a', '#56ce74']).colors(salesReport.length),
+        borderWidth: 5,
+      },
+    ],
+  };
+
+  const chartProductAmount = {
+    labels: salesReport.map(item => item.productName),
+    datasets: [
+      {
+        label: 'Total Sales Amout',
+        data: salesReport.map(item => item.totalSalesAmount),
+        labels: salesReport.map(item => item.productName),
+        backgroundColor: chroma.scale(['#1e4f2a', '#56ce74']).colors(salesReport.length),
+        borderWidth: 5,
+      },
+    ],
   };
 
   return (
@@ -95,18 +118,17 @@ const SalesReport = () => {
         <h1 className="pending-header">No Sales Yet!</h1>
       ) : (
         <>
-          <div className="ordernumber-field">
-            <div className="ordernumber-card">
-              <h2 className="order-title">
-                Total Number of Sales: {totalSalesQuantity}
-              </h2>
+          <div className="sales-chart">
+            <div className="chart-container">
+              <h2 className="sales-highlight">Total Number of Sales: {totalSalesQuantity} </h2>
+              <Pie className="pie-chart" data={chartProductQuantity}/> 
             </div>
-            <div className="ordernumber-card">
-              <h2 className="order-title">
-                Total Sales Amount: Php {totalSalesAmount}
-              </h2>
+            <div className="chart-container">
+              <h2 className="sales-highlight">Total Sales Amount: Php {totalSalesAmount}</h2>
+              <Pie className="pie-chart" data={chartProductAmount} />
             </div>
           </div>
+          
           <table className="user-field">
             <thead>
               <tr>
